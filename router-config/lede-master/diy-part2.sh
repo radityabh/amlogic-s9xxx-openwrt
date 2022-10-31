7a903a1e869fe6565e3d95db01a665d83433d0b5
arqam999 <83278908+arqam999@users.noreply.github.com>
2022-10-31 23:50:32 +0800

    Update diy-part2.sh

--- router-config/lede-master/diy-part2.sh
+++ router-config/lede-master/diy-part2.sh
@@ -8,6 +8,16 @@
 
 # ------------------------------- Main source started -------------------------------
 #
+
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
+# Version Update
+sed -i '/DISTRIB_DESCRIPTION/d' package/base-files/files/etc/openwrt_release
+echo "DISTRIB_DESCRIPTION=' Beta6 build $(TZ=UTC+8 date "+%Y.%m") '" >> package/base-files/files/etc/openwrt_release
+sed -i '/DISTRIB_REVISION/d' package/base-files/files/etc/openwrt_release
+echo "DISTRIB_REVISION='[WSS]'" >> package/base-files/files/etc/openwrt_release
+
 # Modify default theme（FROM uci-theme-bootstrap CHANGE TO luci-theme-material）
 sed -i 's/luci-theme-bootstrap/luci-theme-material/g' ./feeds/luci/collections/luci/Makefile
 
@@ -17,12 +27,6 @@ sed -i 's/LUCI_DEPENDS.*/LUCI_DEPENDS:=\@\(arm\|\|aarch64\)/g' package/lean/luci
 # Add autocore support for armvirt
 sed -i 's/TARGET_rockchip/TARGET_rockchip\|\|TARGET_armvirt/g' package/lean/autocore/Makefile
 
-# Set DISTRIB_REVISION
-sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(date +%Y.%m.%d)'|g" package/lean/default-settings/files/zzz-default-settings
-
-# Modify default IP（FROM 192.168.1.1 CHANGE TO 192.168.31.4）
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
-
 # Modify default root's password（FROM 'password'[$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.] CHANGE TO 'your password'）
 # sed -i 's/root::0:0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' /etc/shadow
 
@@ -31,7 +35,95 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 #
 # ------------------------------- Main source ends -------------------------------
 
+############################################################
+# Remove some net packages
+rm -rf ./feeds/packages/net/https-dns-proxy
+rm -rf ./feeds/packages/net/kcptun
+rm -rf ./feeds/packages/net/shadowsocks-libev
+rm -rf ./feeds/packages/net/xray-core
+rm -rf ./feeds/packages/net/brook
+rm -rf ./feeds/packages/net/chinadns-ng
+rm -rf ./feeds/packages/net/hysteria
+rm -rf ./feeds/packages/net/ssocks
+rm -rf ./feeds/packages/net/trojan
+rm -rf ./feeds/packages/net/trojan-go
+rm -rf ./feeds/packages/net/trojan-plus
+rm -rf ./feeds/packages/net/sagernet-core
+rm -rf ./feeds/packages/net/naiveproxy
+rm -rf ./feeds/packages/net/shadowsocks-rust
+rm -rf ./feeds/packages/net/shadowsocksr-libev
+rm -rf ./feeds/packages/net/simple-obfs
+rm -rf ./feeds/packages/net/tcping
+rm -rf ./feeds/packages/net/v2ray-core
+rm -rf ./feeds/packages/net/v2ray-geodata
+rm -rf ./feeds/packages/net/v2ray-plugin
+rm -rf ./feeds/packages/net/v2raya
+rm -rf ./feeds/packages/net/xray-core
+rm -rf ./feeds/packages/net/xray-plugin
+rm -rf ./feeds/packages/net/dns2socks
+rm -rf ./feeds/packages/net/microsocks
+rm -rf ./feeds/packages/net/ipt2socks
+rm -rf ./feeds/packages/net/pdnsd-alt
+rm -rf ./feeds/packages/net/redsocks2
+
+# Dependencies
+svn export https://github.com/xiaorouji/openwrt-passwall/trunk/brook feeds/packages/net/brook
+svn export https://github.com/xiaorouji/openwrt-passwall/trunk/chinadns-ng feeds/packages/net/chinadns-ng
+svn export https://github.com/xiaorouji/openwrt-passwall/trunk/dns2tcp feeds/packages/net/dns2tcp
+svn export https://github.com/xiaorouji/openwrt-passwall/trunk/hysteria feeds/packages/net/hysteria
+svn export https://github.com/xiaorouji/openwrt-passwall/trunk/ssocks feeds/packages/net/ssocks
+svn export https://github.com/xiaorouji/openwrt-passwall/trunk/sing-box feeds/packages/net/sing-box
+svn export https://github.com/xiaorouji/openwrt-passwall/trunk/trojan-go feeds/packages/net/trojan-go
+svn export https://github.com/xiaorouji/openwrt-passwall/trunk/trojan-plus feeds/packages/net/trojan-plus
+svn export https://github.com/xiaorouji/openwrt-passwall/trunk/sagernet-core feeds/packages/net/sagernet-core
+svn export https://github.com/fw876/helloworld/trunk/naiveproxy feeds/packages/net/naiveproxy
+svn export https://github.com/immortalwrt/packages/trunk/net/shadowsocks-libev feeds/packages/net/shadowsocks-libev
+svn export https://github.com/fw876/helloworld/trunk/shadowsocks-rust feeds/packages/net/shadowsocks-rust
+svn export https://github.com/fw876/helloworld/trunk/shadowsocksr-libev feeds/packages/net/shadowsocksr-libev
+svn export https://github.com/fw876/helloworld/trunk/simple-obfs feeds/packages/net/simple-obfs
+svn export https://github.com/fw876/helloworld/trunk/tcping feeds/packages/net/tcping
+svn export https://github.com/fw876/helloworld/trunk/trojan feeds/packages/net/trojan
+svn export https://github.com/fw876/helloworld/trunk/v2ray-core feeds/packages/net/v2ray-core
+svn export https://github.com/fw876/helloworld/trunk/v2ray-geodata feeds/packages/net/v2ray-geodata
+svn export https://github.com/fw876/helloworld/trunk/v2ray-plugin feeds/packages/net/v2ray-plugin
+svn export https://github.com/fw876/helloworld/trunk/v2raya feeds/packages/net/v2raya
+svn export https://github.com/arqam999/openwrt-passwall/branches/xtls-wss/xray-core feeds/packages/net/xray-core
+svn export https://github.com/fw876/helloworld/trunk/xray-plugin feeds/packages/net/xray-plugin
+svn export https://github.com/fw876/helloworld/trunk/lua-neturl feeds/packages/net/lua-neturl
+svn export https://github.com/immortalwrt/packages/trunk/net/dns2socks feeds/packages/net/dns2socks
+svn export https://github.com/immortalwrt/packages/trunk/net/microsocks feeds/packages/net/microsocks
+svn export https://github.com/immortalwrt/packages/trunk/net/ipt2socks feeds/packages/net/ipt2socks
+svn export https://github.com/immortalwrt/packages/trunk/net/pdnsd-alt feeds/packages/net/pdnsd-alt
+svn export https://github.com/immortalwrt/packages/trunk/net/redsocks2 feeds/packages/net/redsocks2
+svn export https://github.com/immortalwrt/packages/trunk/net/https-dns-proxy feeds/packages/net/https-dns-proxy
+svn export https://github.com/immortalwrt/packages/trunk/net/kcptun feeds/packages/net/kcptun
+svn export https://github.com/kiddin9/openwrt-bypass/trunk/lua-maxminddb feeds/packages/net/lua-maxminddb
+svn export https://github.com/coolsnowwolf/lede/trunk/package/lean/shortcut-fe package/kernel/shortcut-fe
+svn export https://github.com/immortalwrt/packages/trunk/net/dnsforwarder feeds/packages/net/dnsforwarder
+
+############################################################
+# luci-app-passwall
+rm -r ./feeds/luci/applications/luci-app-passwall
+svn export https://github.com/arqam999/openwrt-passwall/branches/luci-nodns/luci-app-passwall feeds/luci/applications/luci-app-passwall
+
+#luci-app-turboacc
+svn export https://github.com/immortalwrt/luci/trunk/applications/luci-app-turboacc feeds/luci/applications/luci-app-turboacc
+
+# Autocore
+svn export https://github.com/immortalwrt/immortalwrt/branches/openwrt-21.02/package/emortal/autocore feeds/packages/utils/autocore
+sed -i 's/"getTempInfo" /"getTempInfo", "getCPUBench", "getCPUUsage" /g' feeds/packages/utils/autocore/files/generic/luci-mod-status-autocore.json
+
+# Coremark
+rm -rf ./feeds/packages/utils/coremark
+svn export https://github.com/immortalwrt/packages/trunk/utils/coremark feeds/packages/utils/coremark
+############################################################
+
+
 # ------------------------------- Other started -------------------------------
+# Clone community packages to package/community
+mkdir package/community
+pushd package/community
+
 # edit Argon
 rm -rf package/lean/luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
@@ -52,9 +144,14 @@ svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/
 # Add luci-app-passwall
 # svn co https://github.com/xiaorouji/openwrt-passwall/trunk package/openwrt-passwall
 
-# Add luci-app-openclash
-svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/openwrt-openclash
-pushd package/openwrt-openclash/tools/po2lmo && make && sudo make install 2>/dev/null && popd
+# Add official OpenClash source
+git clone --depth=1 -b dev https://github.com/vernesong/OpenClash
+
+# Add Adguardhome
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/new/luci-app-adguardhome
+rm -rf ./feeds/packages/net/adguardhome
+svn export https://github.com/openwrt/packages/trunk/net/adguardhome feeds/packages/net/adguardhome
+sed -i '/init/d' feeds/packages/net/adguardhome/Makefile
 
 # Add luci-app-ssr-plus
 # svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/openwrt-ssrplus
@@ -75,6 +172,21 @@ pushd package/openwrt-openclash/tools/po2lmo && make && sudo make install 2>/dev
 
 # Apply patch
 # git apply ../router-config/patches/{0001*,0002*}.patch --directory=feeds/luci
+
+popd
 #
 # ------------------------------- Other ends -------------------------------
+
+# Add extra wireless drivers
+svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-18.06-k5.4/package/kernel/rtl8812au-ac
+svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-18.06-k5.4/package/kernel/rtl8188eu
+svn co https://github.com/immortalwrt/immortalwrt/branches/openwrt-18.06-k5.4/package/kernel/rtl88x2bu
+
+# Add cpufreq
+rm -rf ./feeds/luci/applications/luci-app-cpufreq 
+svn co https://github.com/DHDAXCW/luci-bt/trunk/applications/luci-app-cpufreq ./feeds/luci/applications/luci-app-cpufreq
+ln -sf ./feeds/luci/applications/luci-app-cpufreq ./package/feeds/luci/luci-app-cpufreq
+sed -i 's,1608,1800,g' feeds/luci/applications/luci-app-cpufreq/root/etc/uci-defaults/10-cpufreq
+sed -i 's,2016,2208,g' feeds/luci/applications/luci-app-cpufreq/root/etc/uci-defaults/10-cpufreq
+sed -i 's,1512,1608,g' feeds/luci/applications/luci-app-cpufreq/root/etc/uci-defaults/10-cpufreq
 
